03f5a8257dde808ef7d14d1c0617a3b4b7c81b98
Ireneusz Matysiak <imatysiak@pgs-soft.com>
2020-10-22 20:38:50 +0200

    move resource group

--- providers.tf
+++ providers.tf
@@ -6,16 +6,4 @@ provider "azurerm" {
   features {}
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "irek-example" {
-  name     = "irek-example-resources"
-  location = "West Europe"
-}
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "irek-example" {
-  name                = "irek-example-network"
-  resource_group_name = azurerm_resource_group.irek-example.name
-  location            = azurerm_resource_group.irek-example.location
-  address_space       = ["10.0.0.0/16"]
-}
